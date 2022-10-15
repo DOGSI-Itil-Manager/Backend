450a9c15d3eff19b594d9abe6bb809e75468df18
Bruno Grassano <bgrassano@fi.uba.ar>
2022-10-15 14:51:07 -0300

    Add hardware service tests
    #1

---
+++ src/test/java/com/dogsi/itil/services/HardwareServiceTest.java
@@ -0,0 +1,152 @@
+package com.dogsi.itil.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.Instant;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.dogsi.itil.dto.HardwareDto;
+import com.dogsi.itil.exceptions.ItemNotFoundException;
+import com.dogsi.itil.repositories.HardwareRepository;
+import com.dogsi.itil.services.configuration.HardwareService;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "spring.main.allow-bean-definition-overriding=true")
+@ActiveProfiles("test-h2")
+public class HardwareServiceTest {
+    
+    @Autowired
+    private HardwareRepository repository;
+
+    @Autowired
+    private HardwareService service;
+
+    @AfterEach
+    void tearDown(){
+        repository.deleteAll();
+    }
+
+    @Test
+    void shouldSaveAHardware(){
+        var dto = new HardwareDto();
+        dto.setName("Name");
+        dto.setAdditionDate(Instant.now());
+        dto.setDescription("description");
+        dto.setLocation("Location");
+        dto.setProvider("Provider");
+        dto.setSerialNumber("SerialNumber");
+        dto.setType("type");
+        dto.setPrice(122f);
+        dto.setCapacity(1f);
+
+        service.saveHardware(dto);
+
+        assertEquals(1, repository.count());
+
+        var saved = repository.findAll().get(0);
+        assertEquals("Name", saved.getName());
+    }
+
+    @Test
+    void shouldReturnAllHardware(){
+        var dto = new HardwareDto();
+        dto.setName("Name");
+        dto.setAdditionDate(Instant.now());
+        dto.setDescription("description");
+        dto.setLocation("Location");
+        dto.setProvider("Provider");
+        dto.setSerialNumber("SerialNumber");
+        dto.setType("type");
+        dto.setPrice(122f);
+        dto.setCapacity(1f);
+
+        service.saveHardware(dto);
+        service.saveHardware(dto);
+        service.saveHardware(dto);
+
+        var results = service.getHardware(Pageable.unpaged());
+        assertEquals(3, results.getTotalElements());
+    }
+
+    @Test
+    void shouldThrowAnExceptionIfTheHardwareIsNotFoundWhenUpdating(){
+        var dto = new HardwareDto();
+        dto.setName("Name");
+        dto.setAdditionDate(Instant.now());
+        dto.setDescription("description");
+        dto.setLocation("Location");
+        dto.setProvider("Provider");
+        dto.setSerialNumber("SerialNumber");
+        dto.setType("type");
+        dto.setPrice(122f);
+        dto.setCapacity(1f);
+
+        assertThrows(ItemNotFoundException.class, () -> {
+            service.updateHardware(1L, dto);
+        });
+    }
+
+    @Test
+    void shouldThrowAnExceptionIfTheHardwareIsNotFoundWhenDeleting(){
+        assertThrows(ItemNotFoundException.class, () -> {
+            service.deleteHardware(1L);
+        });
+    }
+
+    @Test
+    void shouldDeleteASoftware(){
+        var dto = new HardwareDto();
+        dto.setName("Name");
+        dto.setAdditionDate(Instant.now());
+        dto.setDescription("description");
+        dto.setLocation("Location");
+        dto.setProvider("Provider");
+        dto.setSerialNumber("SerialNumber");
+        dto.setType("type");
+        dto.setPrice(122f);
+        dto.setCapacity(1f);
+
+        service.saveHardware(dto);
+        assertEquals(1, repository.count());
+        var saved = repository.findAll().get(0);
+
+        service.deleteHardware(saved.getId());
+
+        assertEquals(0, repository.count());
+    }
+
+
+    @Test
+    void shouldUpdateASoftware(){
+        var dto = new HardwareDto();
+        dto.setName("Name");
+        dto.setAdditionDate(Instant.now());
+        dto.setDescription("description");
+        dto.setLocation("Location");
+        dto.setProvider("Provider");
+        dto.setSerialNumber("SerialNumber");
+        dto.setType("type");
+        dto.setPrice(122f);
+        dto.setCapacity(1f);
+
+        service.saveHardware(dto);
+        assertEquals(1, repository.count());
+        var saved = repository.findAll().get(0);
+
+        dto.setName("Name2");
+        
+        service.updateHardware(saved.getId(), dto);
+
+        assertEquals(1, repository.count());
+        saved = repository.findAll().get(0);
+        assertEquals("Name2", saved.getName());
+
+    }
+}
