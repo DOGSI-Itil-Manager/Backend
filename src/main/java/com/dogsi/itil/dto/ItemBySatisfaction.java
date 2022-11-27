8c1367bf4ab0d3ef644713093d1b6df57a7e8b0b
Bruno Grassano <bgrassano@fi.uba.ar>
2022-11-27 15:06:55 -0300

    Change null value in satisfaction metric

--- src/main/java/com/dogsi/itil/dto/ItemBySatisfaction.java
+++ src/main/java/com/dogsi/itil/dto/ItemBySatisfaction.java
@@ -2,14 +2,19 @@ package com.dogsi.itil.dto;
 
 import com.dogsi.itil.domain.Satisfaction;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Getter
 public class ItemBySatisfaction {
-    private Satisfaction field;
+    private String field;
     private long count;
+
+    public ItemBySatisfaction(Satisfaction field, long count) {
+        this.field = field == null ? "SIN_CALIFICAR" : field.name();
+        this.count = count;
+    }
 }
